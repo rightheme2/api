2725e1e4b3104904f12cbcf33cba4c7d30dfa409
rightheme2 <rightheme.ir@gmail.com>
2025-02-25 05:35:27 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php get_header();
+get_template_part('inc/parts/story');
+get_template_part('inc/parts/top-slider');
+get_template_part('inc/parts/top-categorys');
+get_template_part('inc/parts/hit-products');
+get_template_part('inc/parts/offer-products');
+get_template_part('inc/parts/new-products');
+get_template_part('inc/parts/random-products');
+get_template_part('inc/parts/shuffle-products');
+get_template_part('inc/parts/index-social');
+get_footer(); ?>
